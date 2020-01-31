4b76bcc5b5aa896c50ee0c4137fe1a3079bb4e35
Matt Lehrer <lehrerm@gmail.com>
2020-01-30 17:02:39 -0800

    setup stream

---
+++ getstream.js
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv';
+dontenv.config();
+
+exports.config = {
+  /**
+   * GetStream.io API key
+   */
+  apiKey: process.env.STREAM_KEY,
+
+  /**
+   * GetStream.io API Secret
+   */
+  apiSecret: process.env.STREAM_SECRET,
+
+  /**
+   * GetStream.io API App ID
+   */
+  apiAppId: process.env.STREAM_APP_ID,
+
+  /**
+   * GetStream.io API Location
+   */
+  apiLocation: '',
+
+  /**
+   * GetStream.io User Feed slug
+   */
+  userFeed: 'user',
+
+  /**
+   * GetStream.io Notification Feed slug
+   */
+  notificationFeed: 'notification',
+
+  newsFeeds: {
+    /**
+     * GetStream.io Flat Feed slug
+     */
+    flat: 'flat',
+
+    /**
+     * GetStream.io Aggregated Feed slug
+     */
+    aggregated: 'aggregated',
+  },
+};
